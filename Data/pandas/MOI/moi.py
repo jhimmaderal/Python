6adedd62f941ae36b40fa2254908fa51363377f9
Jim Paul Maderal <jhimmaderal@gmail.com>
2024-02-06 17:24:38 +0300

    Adding Website

--- Data/pandas/MOI/moi.py
+++ Data/pandas/MOI/moi.py
@@ -4,11 +4,11 @@ import time
 
 def checkFiles():
     dataframes = {}
-    excelFile = pd.ExcelFile("Z:/Jim/MOI/UploadData.xlsx")
+    excelFile = pd.ExcelFile("Z:/Jim/MOI/PSMS/UploadData.xlsx")
     sheetNames = excelFile.sheet_names
     for sheetName in sheetNames:
         dataframes[sheetName] = pd.read_excel(excelFile, sheet_name=sheetName)
-    work = dataframes["Sample"]
+    work = dataframes["MasterBC"]
     ap = dataframes["001"]
     al = dataframes["002"]
     ms = dataframes["003"]
@@ -16,6 +16,7 @@ def checkFiles():
     # pd.ExcelFile("UploadData.xlsx").sheet_names - Check Sheetnames
 
     def mergeFiles():
+
         columns = [
             "trdate",
             "doc_type",
@@ -25,7 +26,9 @@ def checkFiles():
             "loc_id",
             "qty",
             "price",
-            "refno",
+            "tr_no",
+            "tr_no",
+            "no_of_bills",
         ]
         df1 = work.merge(ap, left_on="barcode", right_on="barcode", how="left")
         df2 = work.merge(al, left_on="barcode", right_on="barcode", how="left")
@@ -48,19 +51,19 @@ def checkFiles():
         if "y" in validate:
             try:
                 with pd.ExcelWriter(
-                    "Z:/Jim/MOI/LogSales.xlsx", engine="openpyxl"
+                    "Z:/Jim/MOI/PSMS/SalesLog.xlsx", engine="openpyxl"
                 ) as writer:
-                    df1.to_excel(writer, "LogSales", index=False)
+                    df1.to_excel(writer, "SalesLog", index=False)
                     df2.to_excel(
                         writer,
-                        "LogSales",
+                        "SalesLog",
                         startrow=(line1 + 1),
                         header=False,
                         index=False,
                     )
                     df3.to_excel(
                         writer,
-                        "LogSales",
+                        "SalesLog",
                         startrow=(line1 + line2 + 1),
                         header=False,
                         index=False,
@@ -69,7 +72,7 @@ def checkFiles():
                 print("\nFile Saved Succesfully \n")
             except PermissionError:
                 print(
-                    "Permission denied: LogSales.xlsx. Please close the file before running the script again."
+                    "Permission denied: SalesLog.xlsx. Please close the file before running the script again."
                 )
             runAgain = input("Run script again ? Y/N: ")
             if "y" in runAgain:
